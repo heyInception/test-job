f853f7f778ad6a4f0b38f63c638cbde0e83d8ca6
heyInception <antonschastlivc@gmail.com>
2021-09-30 01:37:51 +0400

    build

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,1 @@
 "use strict";
-
-console.log('global');
-"use strict";
-
-console.log('maxgraph');
-/**
-  * название функции
-  *
-  * @param {number} first - первое число
-  * @returns {number}
-  */
-"use strict";
-//# sourceMappingURL=main.js.map
